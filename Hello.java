467d5012194cbda27b5b198c9f075abe9ff08dcc
softwaretoolsmonster <97654705+softwaretoolsmonster@users.noreply.github.com>
2023-09-27 12:23:45 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Hello {
+  public static void main(String[] args) throws IOException {
+    String content = Files.readString(Path.of(args[0]), StandardCharsets.UTF_8);    
+    System.out.println(content);
+  }
+}
---
+++ WhereAmI.java
@@ -0,0 +1,8 @@
+class WhereAmI {
+  public static void main(String[] args) {
+    System.out.println(System.getProperty("os.name"));
+    System.out.println(System.getProperty("user.name"));
+    System.out.println(System.getProperty("user.home"));
+    System.out.println(System.getProperty("user.dir"));
+  }
+}
